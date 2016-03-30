fb52a1eb2ed0be66042f8780146642acea16a322
Skaiiwalker <lbar8299@740s-pl-369.local>
2016-03-30 09:11:30 -0600

    saves

---
+++ src/ctec/model/ImaginaryWallet.java
@@ -0,0 +1,11 @@
+package ctec.model;
+
+public class ImaginaryWallet extends Wallet 
+{
+	public ImaginaryWallet()
+	{
+		this.setMoney(1000000);
+		this.setOwnerName("Luke");
+		this.setTopCard("Brighton Season Pass");
+	}
+}
